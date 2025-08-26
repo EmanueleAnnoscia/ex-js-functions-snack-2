a0cef15c6311e1e6cd794f3c8f1a7b2c239a0467
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-08-26 11:58:31 +0200

    throttled function molto figa

--- ex-js-functions-snack-2.js
+++ ex-js-functions-snack-2.js
@@ -75,37 +75,64 @@
 
 // eseguiEFerma('ciao', 1000, 5000);
 
-//snack 8 
-function contoAllaRovescia(counter, timeSetInterval){
-    let stop = (counter+1) * 1000;
-    const id =  setInterval(()=> console.log(counter --), timeSetInterval)
+// //snack 8 
+// function contoAllaRovescia(counter, timeSetInterval){
+//     let stop = (counter+1) * 1000;
+//     const id =  setInterval(()=> console.log(counter --), timeSetInterval)
 
-    setTimeout(()=> {
-        clearInterval(id) 
-        console.log('Intervallo fermato!') 
-    }, stop);
+//     setTimeout(()=> {
+//         clearInterval(id) 
+//         console.log('Intervallo fermato!') 
+//     }, stop);
 
-}
+// }
 
-contoAllaRovescia(10, 1000);
+// contoAllaRovescia(10, 1000);
 
-//snack 9
-function sequenzaOperazioni(array, numeroIntervallo){
-    let index= 0;
+// //snack 9
+// function sequenzaOperazioni(array, numeroIntervallo){
+//     let index= 0;
 
-    const id = setInterval(()=> {
-        console.log(array[index]);
-        index ++;
+//     const id = setInterval(()=> {
+//         console.log(array[index]);
+//         index ++;
 
-        if (index > array.length){
-            clearInterval(id);
-            console.log('Sequenza temrinata!')
-        }
-    }, numeroIntervallo)
+//         if (index > array.length){
+//             clearInterval(id);
+//             console.log('Sequenza temrinata!')
+//         }
+//     }, numeroIntervallo)
 
-}
+// }
 
-sequenzaOperazioni(["operazione", "operazione1", "operazione2", "operazione3"], 2000)
+// sequenzaOperazioni(["operazione", "operazione1", "operazione2", "operazione3"], 2000)
 
 //snack10
 
+function somma(numero1, numero2){
+     return numero1 + numero2;
+ }
+
+ let square = (numero) => numero * numero;
+
+function creaThrottler( callback ,limite){
+    let ultimoTempo = 0;
+
+    return function(...args){
+        const adesso = Date.now();
+        if(adesso - ultimoTempo>= limite){
+            ultimoTempo = adesso;
+            return callback(...args)
+        }
+    };
+
+}
+
+const sommaThrottled = creaThrottler(somma, 5000)
+// const squareThrottled = creaThrottler (square, 5000)
+
+// console.log(sommaThrottled(5,8));
+// console.log(squareThrottled(13));
+// setTimeout(() => console.log(sommaThrottled(2, 3)), 6000); 
+setInterval(()=> console.log(sommaThrottled(80,20)), 1000)
+// setTimeout(()=> console.log(squareThrottled(13)), 5000)
